a5dd9b933b6287fbf2fd239a3b2698ea3b0cc182
renhai-lab <3152498252@qq.com>
2024-06-08 23:13:11 +0800

    DEBUG

--- replace_username.sh
+++ replace_username.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 从环境变量中读取用户名
-USERNAME="${ALIYUN_REGISTRY_USER}"
+HUB_NAME="${ALIYUN_HUB_NAME}"
 
 
 # 替换 trigger.txt 中的占位符
-sed -i "s/USERNAME_PLACEHOLDER/$USERNAME/g" trigger.txt
+sed -i "s/HUB_NAME_PLACEHOLDER/$HUB_NAME/g" trigger.txt
